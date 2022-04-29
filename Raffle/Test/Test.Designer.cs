0b4dfd02cebc7c798f59b19badef2e5b6c54314d
GRWolfy <granthreggwolf@gmail.com>
2022-04-29 11:24:01 +0800

    l

--- Raffle/Test/Test.Designer.cs
+++ Raffle/Test/Test.Designer.cs
@@ -30,23 +30,34 @@
         {
             this.datagridviewRaffle = new System.Windows.Forms.DataGridView();
             this.btnSelectExcel = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtFileName = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.datagridStudents = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnSaveToDatabase = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.datagridviewRaffle)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.datagridStudents)).BeginInit();
             this.SuspendLayout();
             // 
             // datagridviewRaffle
             // 
+            this.datagridviewRaffle.AllowUserToAddRows = false;
+            this.datagridviewRaffle.AllowUserToDeleteRows = false;
             this.datagridviewRaffle.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.datagridviewRaffle.Location = new System.Drawing.Point(149, 41);
+            this.datagridviewRaffle.Location = new System.Drawing.Point(65, 41);
             this.datagridviewRaffle.Name = "datagridviewRaffle";
             this.datagridviewRaffle.RowTemplate.Height = 25;
-            this.datagridviewRaffle.Size = new System.Drawing.Size(590, 303);
-            this.datagridviewRaffle.TabIndex = 0;
+            this.datagridviewRaffle.Size = new System.Drawing.Size(692, 307);
+            this.datagridviewRaffle.TabIndex = 4;
             // 
             // btnSelectExcel
             // 
-            this.btnSelectExcel.Location = new System.Drawing.Point(569, 376);
+            this.btnSelectExcel.Location = new System.Drawing.Point(447, 370);
             this.btnSelectExcel.Name = "btnSelectExcel";
             this.btnSelectExcel.Size = new System.Drawing.Size(114, 44);
             this.btnSelectExcel.TabIndex = 1;
@@ -54,37 +65,98 @@
             this.btnSelectExcel.UseVisualStyleBackColor = true;
             this.btnSelectExcel.Click += new System.EventHandler(this.btnSelectExcel_Click);
             // 
-            // textBox1
+            // txtFileName
             // 
-            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox1.Location = new System.Drawing.Point(292, 376);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(261, 39);
-            this.textBox1.TabIndex = 2;
+            this.txtFileName.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtFileName.Location = new System.Drawing.Point(170, 370);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(261, 39);
+            this.txtFileName.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(167, 376);
+            this.label1.Location = new System.Drawing.Point(45, 370);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(108, 28);
             this.label1.TabIndex = 3;
             this.label1.Text = "File name:";
+            // 
+            // datagridStudents
+            // 
+            this.datagridStudents.AllowUserToAddRows = false;
+            this.datagridStudents.AllowUserToDeleteRows = false;
+            this.datagridStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.datagridStudents.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6});
+            this.datagridStudents.Location = new System.Drawing.Point(65, 41);
+            this.datagridStudents.Name = "datagridStudents";
+            this.datagridStudents.RowTemplate.Height = 25;
+            this.datagridStudents.Size = new System.Drawing.Size(692, 307);
+            this.datagridStudents.TabIndex = 5;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "ID NUMBER";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "FIRS NAME";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "LAST NAME";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "COURSE";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "YEAR LEVEL";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "CAMPUS";
+            this.Column6.Name = "Column6";
+            // 
+            // btnSaveToDatabase
+            // 
+            this.btnSaveToDatabase.Location = new System.Drawing.Point(621, 370);
+            this.btnSaveToDatabase.Name = "btnSaveToDatabase";
+            this.btnSaveToDatabase.Size = new System.Drawing.Size(105, 44);
+            this.btnSaveToDatabase.TabIndex = 6;
+            this.btnSaveToDatabase.Text = "Save to Database";
+            this.btnSaveToDatabase.UseVisualStyleBackColor = true;
+            this.btnSaveToDatabase.Click += new System.EventHandler(this.btnSaveToDatabase_Click);
             // 
             // Test
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSaveToDatabase);
+            this.Controls.Add(this.datagridStudents);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtFileName);
             this.Controls.Add(this.btnSelectExcel);
             this.Controls.Add(this.datagridviewRaffle);
             this.Name = "Test";
             this.Text = "Test";
             this.Load += new System.EventHandler(this.Test_Load);
             ((System.ComponentModel.ISupportInitialize)(this.datagridviewRaffle)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.datagridStudents)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -94,7 +166,15 @@
 
         private DataGridView datagridviewRaffle;
         private Button btnSelectExcel;
-        private TextBox textBox1;
+        private TextBox txtFileName;
         private Label label1;
+        private DataGridView datagridStudents;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private DataGridViewTextBoxColumn Column3;
+        private DataGridViewTextBoxColumn Column4;
+        private DataGridViewTextBoxColumn Column5;
+        private DataGridViewTextBoxColumn Column6;
+        private Button btnSaveToDatabase;
     }
 }
--- Raffle/Test/Test.cs
+++ Raffle/Test/Test.cs
@@ -7,11 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using ExcelDataReader;
 
 namespace Raffle.Test
 {
     public partial class Test : Form
     {
+        const int MAX_COL = 6;
+
         public Test()
         {
             InitializeComponent();
@@ -26,24 +31,77 @@ namespace Raffle.Test
 
         private void Test_Load(object sender, EventArgs e)
         {
-            viewStudentsDb();
+            //viewStudentsDb();
         }
 
-        /*private void btnSelectExcel_Click(object sender, EventArgs e)
+        private void btnSelectExcel_Click(object sender, EventArgs e)
         {
             OpenFileDialog dlg = new OpenFileDialog();
             dlg.Title = "Select file";
-            dlg.InitialDirectory = @"c:\";
             dlg.FileName = txtFileName.Text;
-            dlg.Filter = "Excel Sheet(*.xls)|*.xls|All Files(*.*)|*.*";
+            dlg.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm";
             dlg.FilterIndex = 1;
             dlg.RestoreDirectory = true;
+
             if (dlg.ShowDialog() == DialogResult.OK)
             {
                 txtFileName.Text = dlg.FileName;
-                Import();
-                Application.DoEvents();
+                excelFileReader(dlg.FileName.ToString());
             }
-        }*/
+        }
+
+        public void excelFileReader(string path)
+        {
+            int count = 0;
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            var stream = File.Open(path, FileMode.Open, FileAccess.Read);
+            var reader = ExcelReaderFactory.CreateReader(stream);
+            var result = reader.AsDataSet();
+            var tables = result.Tables.Cast<DataTable>();
+
+            datagridviewRaffle.Visible = false;
+
+            foreach (DataTable table in tables)
+            {
+                datagridviewRaffle.DataSource = table;
+            }
+
+            viewStudents(datagridviewRaffle.Rows.Count);
+        }
+
+        public void viewStudents(int count) {
+            for (int i = 1; i < count; i++)
+            {
+                datagridStudents.Rows.Add(datagridviewRaffle.Rows[i].Cells[0].Value.ToString(),
+                    datagridviewRaffle.Rows[i].Cells[1].Value.ToString(),
+                    datagridviewRaffle.Rows[i].Cells[2].Value.ToString(),
+                    datagridviewRaffle.Rows[i].Cells[3].Value.ToString(),
+                    datagridviewRaffle.Rows[i].Cells[4].Value.ToString(),
+                    datagridviewRaffle.Rows[i].Cells[5].Value.ToString());
+            }
+        }
+
+        private void btnSaveToDatabase_Click(object sender, EventArgs e)
+        {
+            int count = datagridStudents.Rows.Count;
+
+            for (int i = 0; i < count; i++)
+            {
+                int idnumber = Convert.ToInt32(datagridStudents.Rows[i].Cells[0].Value);
+                string firstname = datagridStudents.Rows[i].Cells[1].Value.ToString();
+                string lastname = datagridStudents.Rows[i].Cells[2].Value.ToString();
+                string course = datagridStudents.Rows[i].Cells[3].Value.ToString();
+                string yearlevel = datagridStudents.Rows[i].Cells[4].Value.ToString();
+                string campus = datagridStudents.Rows[i].Cells[5].Value.ToString();
+
+                Connection.DB();
+                Function.gen = "INSERT INTO students(idnumber, firstname, lastname, course, yearlevel, campus) VALUES('" + idnumber + "', '" + firstname + "', '" + lastname + "', '" + course + "', '" + yearlevel + "', '" + campus + "')";
+                Function.command = new SqlCommand(Function.gen, Connection.con);
+                Function.command.ExecuteNonQuery();
+                Connection.con.Close();
+            }
+
+            MessageBox.Show("Students information saved to database.", "Saved", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
     }
 }
